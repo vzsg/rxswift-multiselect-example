524f5bd6b67f54c8863dd73df1a96060afe5939a
Zsolt Váradi <vzsg@users.noreply.github.com>
2017-12-16 16:23:27 +0100

    Initial Commit

---
+++ MultiSelectButtons/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  MultiSelectButtons
+//
+//  Created by Zsolt Váradi on 2017. 12. 16..
+//  Copyright © 2017. Ouroboros. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
